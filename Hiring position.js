2edf5fd0e40ea39a619d00e9c6dabdb08b0c5a2f
Wenjing Zhang (BJ Fesco) <a-wezhan@microsoft.com>
2019-11-11 11:03:31 +0800

    11.11

--- Hiring position.js
+++ Hiring position.js
@@ -1,9 +1,20 @@
 export default `
-# Microsoft hiring 
+# Microsoft hiring Position
+ ***Last update 2019.11.11***
+ 
+ This is Wenjing Zhang from Microsoft recruiting team, and here is my Github pages.  
+ If you are interested in any of these positions and you can contact me via [Linkedin](https://www.linkedin.com/in/wenjing-zhang-154221163/)/Email:a-wezhan@microsoft.com/ Wechat:13182615468
+
 ## Suzhou 
-1. Software engineer
+1. Software engineer  
 
   We are hiring **UX dev** for __[Bing Ads](http://www.baidu.com)__ team!
+2. Hardware engineer  
+    
+  Senior Factory Management Manufacturing Engineer-Mechanical  
+  
+  Senior enclosure development engineer  [Job link](https://careers.microsoft.com/us/en/job/677518/SENIOR-ENCLOSURE-DEVELOPMENT-ENGINEER)
+   senior
 
 
   [http://www.baidu.com](http://www.baidu.com)
@@ -13,4 +24,5 @@ export default `
 
 ## Shanghai
 ## Beijing
+
 `;
