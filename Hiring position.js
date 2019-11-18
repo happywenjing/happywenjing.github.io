9e3938a2a272364e3fe31b951ac4234edb17c3e0
Wenjing Zhang (BJ Fesco) <a-wezhan@microsoft.com>
2019-11-18 17:43:46 +0800

    11.18.1

--- Hiring position.js
+++ Hiring position.js
@@ -1,7 +1,7 @@
 export default `
 ![Microsoft](https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31)
 # Microsoft Openings
- ***Last update 2019.11.15***
+ ***Last update 2019.11.18***
  
  This is Wenjing Zhang from Microsoft recruiting team, and here is my Github pages.  
  If you are interested in any of these positions, you can contact me via [Linkedin](https://www.linkedin.com/in/wenjing-zhang-154221163/)/Email:a-wezhan@microsoft.com/ Wechat:13182615468
@@ -35,6 +35,10 @@ export default `
   Firmware development Engineer-Cloud Sever Infrastructure-----BIOS/UEFI/BMC(Baseboard management Controller) develop experiences [Job link](https://careers.microsoft.com/us/en/job/714891/Firmware-Development-Engineer-Growth-Mindset-CSI-Azure-Cloud-Server-Infrastructure-AzureHardware)  
   
   Senior Hardware Engineer-Cloud Server----Server or Sever rack related debug or trouble shooting experiences.[Job link](https://careers.microsoft.com/us/en/job/742553/Senior-Hardware-Engineer-Cloud-Server)  
+
+2. Software engineer  
+
+  System Software Engineer----Computer vision, face recognition or AI related.[Job link](https://careers.microsoft.com/us/en/job/731505/System-Software-Engineer)
   
   
 
