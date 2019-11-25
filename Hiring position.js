15ed55440dc67377c8d85629f7347916b3492a22
Wenjing Zhang (BJ Fesco) <a-wezhan@microsoft.com>
2019-11-25 18:09:56 +0800

    11.25.1

--- Hiring position.js
+++ Hiring position.js
@@ -1,13 +1,60 @@
 export default `
 ![Microsoft](https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31)
 # Microsoft Openings
- ***Last update 2019.11.18***
+ ***Last update 2019.11.25***
  
  This is Wenjing Zhang from Microsoft recruiting team, and here is my Github pages.  
  If you are interested in any of these positions, you can contact me via [Linkedin](https://www.linkedin.com/in/wenjing-zhang-154221163/)/Email:a-wezhan@microsoft.com/ Wechat:13182615468
 
 ## Suzhou 
-1. Software engineer    
+1. Software engineer 
+
+  Microsoft 365 team  
+
+  Microsoft 365 Big Data and Compliance Engineering team [Job Link](https://careers.microsoft.com/us/en/job/749919/Software-Engineer-SEII-O365-Data-insights)  
+
+  HA/Store team,1+ years experience [Job link](https://careers.microsoft.com/us/en/job/696901/Software-Engineer)  
+
+  Migration team: O365 data migration, 1+ years expereince， backend dev [Job Link](https://careers.microsoft.com/us/en/job/737537/Software-Engineer-SEII-O365-Migration)  
+
+  Engineering system  [Job link](https://careers.microsoft.com/us/en/job/721625/Software-Engineer)
+
+  Performance team， 3+ years experience [Job Link](https://careers.microsoft.com/us/en/job/749924/Software-Engineer-SEII-Sr-SE-Performance)
+
+  EverGreen [Job Link](https://careers.microsoft.com/us/en/job/689115/Software-Engineer-SEII-O365-Evergreen)  
+   
+  Core Fabric-Mornitoring, Enterprise Mail Service-Exchange server montirong system [Job link](https://careers.microsoft.com/us/en/job/721620/Software-Engineer-II-Sr-SE-O365-Monitoring)
+
+  App Fabric--Fleet,3+ years experience [Job link](https://careers.microsoft.com/us/en/job/749933/Software-Engineer-SEII-Sr-SE-O365-Fleet)
+
+  OS&SKU, 1+ years experience [Job Link](https://careers.microsoft.com/us/en/job/686832/Software-Engineer-SEII-O365-OS-SKU)
+   
+  O365 drop box：management platform [Job link](https://careers.microsoft.com/us/en/job/724253/Software-Engineer)  
+
+  HA & Substrate Management Platform and Azure convergence,3+ years experience [Job Link](https://careers.microsoft.com/us/en/job/749923/Sr-Data-Applied-Scientist-Data-Scientist-O365-HA-Store)
+
+  O365 Protocols [Job Link](https://careers.microsoft.com/us/en/job/742501/Software-Engineer-II-Sr-SDE-Protocols)
+
+  AD TEAM: Directory, distributed system services or NoSQL storage is nice to have background [Job link](https://careers.microsoft.com/us/en/job/725112/Software-Engineer)  
+  
+  Mobile app development， 3 years+ [Job Link](https://careers.microsoft.com/us/en/job/730644/Mobile-Engineer-Android-iOS-O365)  
+
+  IDEAs/CFR:M365 Insights, Data Engineering & Analytics team.[Job link](https://careers.microsoft.com/us/en/job/744876/Software-Engineer-SEII-O365-IDEAs)  
+
+  Information Protection,Anti-spam and security features for the enterprise-level service known as Exchange Online Protection (EOP) ,2 years+ working experience.[Job Link](https://careers.microsoft.com/us/en/job/724261/Software-Engineer)  
+
+  
+
+
+
+
+
+
+
+
+
+
+
 
 
   
