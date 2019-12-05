60e72f83c45520338f48f225ada1010ee8c2738f
Wenjing Zhang (BJ Fesco) <a-wezhan@microsoft.com>
2019-12-05 22:27:42 +0800

    12.5

--- jd/content.js
+++ jd/content.js
@@ -1,13 +1,37 @@
 export default `
 ![Microsoft](https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31)
-# Microsoft Openings
- ***Last update 2019.11.29***
+# Microsoft Openings-Product Engineer- TPM
+ ***Last update 2019.12.5***
  
  This is Wenjing Zhang from Microsoft recruiting team, and here is my Github pages.  
  If you are interested in any of these positions, you can contact me via [Linkedin](https://www.linkedin.com/in/wenjing-zhang-154221163/)/Email:a-wezhan@microsoft.com/ Wechat:13182615468
 
-## Shenzhen
-1. Hardware engineer  
+## Location: Suzhou  
 
-  Sr. Technical Operation Engineer-Factory Management----PSU or cable connector related [Job Link](https://careers.microsoft.com/us/en/job/700303/Sr-Technical-Operation-Engineer-Factory-Management)
+**Overview**  
+
+This position understands the engineering design of Microsoft products and ensures that the products are shipped in highest quality during mass production. Successful candidate may lead a group of Engineers to drive yield improvement and quality improvement by understanding and resolving product failures and early returns.  In addition, this role requires detail management of business metrics within a product cycle specifically during mass production. This position will heavily interact with the contract manufacturers, and must be skilled in a wide range of activities from designing and root cause analysis to the manufacturing engineering and managing experiments with outsourced contract manufacturers located in China.
+
+**Responsibilities**  
+
+In order to be successful in this position, this individual must effectively collaborate with both the contract manufacturer and a variety of peer groups across the organization, including, but not limited to, Factory Management, Supplier Quality Engineering, Manufacturing Test Engineering, Quality, and Hardware Development.
+
+**Description:**  
+
+• Ensure no quality excursion in mass production of products  
+• Understand product designs and provide engineering support and leadership at engineering builds  
+• Lead a group of engineers to track production product problems and timely conduct fault isolation and failure analysis  
+• Being the engineering hub during mass production and proactively work with cross-groups to drive continuous improvement  
+• Provide professional recommendation to Quality for potential product quality issues.   
+
+**MACH CHINA**
+
+
+
+Microsoft is an equal opportunity employer. All qualified applicants will receive consideration for employment without regard to age, ancestry, color, family or medical care leave, gender identity or expression, genetic information, marital status, medical condition, national origin, physical or mental disability, political affiliation, protected veteran status, race, religion, sex (including pregnancy), sexual orientation, or any other characteristic protected by applicable laws, regulations and ordinances. 
+
+
+
+Benefits/perks listed below may vary depending on the nature of your employment with Microsoft and the country where you work.
+
 `;
