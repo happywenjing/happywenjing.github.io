6a5ee7cd706ab1eb87c6c41b1b4272d70ad0af10
Wenjing Zhang (BJ Fesco) <a-wezhan@microsoft.com>
2020-02-20 15:38:25 +0800

    20.20.2.12

--- jd/content.js
+++ jd/content.js
@@ -6,6 +6,18 @@ export default `
  This is Wenjing Zhang from Microsoft recruiting team, and here is my Github pages.    
  If you are interested in any of these positions, you can contact me via [Linkedin](https://www.linkedin.com/in/wenjing-zhang-154221163/)/Email:a-wezhan@microsoft.com/ Wechat:13182615468  
   
+ # Senior SW/FW engineer  
+
+  Senior Software Development Engineer----develop Automation platform   [Job link](https://careers.microsoft.com/us/en/job/733317/Senior-Software-Development-Engineer)
+
+  Senior Software Development Engineer-Dispaly----Debug or develop for Display software related. [Job Link](https://careers.microsoft.com/us/en/job/733365/Senior-Software-Development-Engineer-Display)
+ 
+  Senior Software Development Engineer-RF---- Software/Firmware Radio Frequency (RF) team [Job Link](https://careers.microsoft.com/us/en/job/723072/SDE-2-RF)  
+
+  Senior Software Engineer_Design Verification----Android OR windows related experiences [Job link](https://careers.microsoft.com/us/en/job/712684/Senior-Software-Engineer-Design-Verification)  
+
+  MTE-Senior software development engineer---- Android system experiences.[Job Link](https://careers.microsoft.com/us/en/job/759956/MTE-Senior-Software-Development-Engineer-China) 
+
 # Senior Coatings Engineer  
 ## Location: Suzhou  
   
