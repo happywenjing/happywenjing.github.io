27b9dcbb169bb220a55b31e35ebf315d76246586
Wenjing Zhang (BJ Fesco) <a-wezhan@microsoft.com>
2020-04-08 10:01:54 +0800

    20.4.8

--- index/content.js
+++ index/content.js
@@ -6,109 +6,28 @@ export default `
  This is Wenjing Zhang from Microsoft recruiting team, and here is my Github pages.  
  If you are interested in any of these positions, you can contact me via [Linkedin](https://www.linkedin.com/in/wenjing-zhang-154221163/)/Email:a-wezhan@microsoft.com/ Wechat:13182615468
 
-## Suzhou 
-1. Software engineer 
-
-  Microsoft 365 team  
-
-  Microsoft 365 Big Data and Compliance Engineering team [Job Link](https://careers.microsoft.com/us/en/job/749919/Software-Engineer-SEII-O365-Data-insights)  
-
-  HA/Store team, 1+ years experience [Job link](https://careers.microsoft.com/us/en/job/696901/Software-Engineer)  
-
-  Migration team: O365 data migration, 1+ years expereince， backend dev [Job Link](https://careers.microsoft.com/us/en/job/737537/Software-Engineer-SEII-O365-Migration)  
-
-  Engineering system  [Job link](https://careers.microsoft.com/us/en/job/721625/Software-Engineer)
-
-  Performance team, 3+ years experience [Job Link](https://careers.microsoft.com/us/en/job/749924/Software-Engineer-SEII-Sr-SE-Performance)
-
-  EverGreen [Job Link](https://careers.microsoft.com/us/en/job/689115/Software-Engineer-SEII-O365-Evergreen)  
-   
-  Core Fabric-Mornitoring, Enterprise Mail Service-Exchange server montirong system [Job link](https://careers.microsoft.com/us/en/job/721620/Software-Engineer-II-Sr-SE-O365-Monitoring)
-
-  App Fabric--Fleet, 3+ years experience. [Job link](https://careers.microsoft.com/us/en/job/749933/Software-Engineer-SEII-Sr-SE-O365-Fleet)
-
-  OS&SKU, 1+ years experience. [Job Link](https://careers.microsoft.com/us/en/job/686832/Software-Engineer-SEII-O365-OS-SKU)
-   
-  O365 drop box: management platform. [Job link](https://careers.microsoft.com/us/en/job/724253/Software-Engineer)  
-
-  HA & Substrate Management Platform and Azure convergence,3+ years experience. [Job Link](https://careers.microsoft.com/us/en/job/749923/Sr-Data-Applied-Scientist-Data-Scientist-O365-HA-Store)
-
-  O365 Protocols [Job Link](https://careers.microsoft.com/us/en/job/742501/Software-Engineer-II-Sr-SDE-Protocols)
-
-  AD TEAM: Directory, distributed system services or NoSQL storage is nice to have background [Job link](https://careers.microsoft.com/us/en/job/725112/Software-Engineer)  
-  
-  Mobile app development, 3 years+ [Job Link](https://careers.microsoft.com/us/en/job/730644/Mobile-Engineer-Android-iOS-O365)  
-
-  IDEAs/CFR:M365 Insights, Data Engineering & Analytics team.[Job link](https://careers.microsoft.com/us/en/job/744876/Software-Engineer-SEII-O365-IDEAs)  
-
-  Information Protection, Anti-spam and security features for the enterprise-level service known as Exchange Online Protection (EOP) ,2 years+ working experience.[Job Link](https://careers.microsoft.com/us/en/job/724261/Software-Engineer)  
-
-  
-2. Hardware engineer  
-    
-  Senior Factory Management Manufacturing Engineer-Mechanical [Job Link] (https://careers.microsoft.com/us/en/job/748910/Senior-Factory-Management-Engineer) 
-
-  Senior Factory Management Engineering-Polymer [Job link](https://careers.microsoft.com/us/en/job/739684/Manager-Factory-Management-Engineering)
-
-  Senior Factory Management Engineer_ Optical Engineering----Camera related. [Job Link](https://careers.microsoft.com/us/en/job/681921/Senior-Factory-Management-Engineer-Optical-Engineering)
-  
-  Senior Operations Program Manager----Manufacturing Test program experiences[Job Link](https://careers.microsoft.com/us/en/job/737552/Sr-Operations-Program-Manager)
-
-  Senior enclosure development engineer  [Job link](https://careers.microsoft.com/us/en/job/677518/SENIOR-ENCLOSURE-DEVELOPMENT-ENGINEER)
-  
-  Senior Software Development Engineer----develop Automation platform   [Job link](https://careers.microsoft.com/us/en/job/733317/Senior-Software-Development-Engineer)
-
-  Senior Software Development Engineer-Dispaly----Debug or develop for Display software related. [Job Link](https://careers.microsoft.com/us/en/job/733365/Senior-Software-Development-Engineer-Display)
- 
-  Senior Software Development Engineer-RF---- Software/Firmware Radio Frequency (RF) team [Job Link](https://careers.microsoft.com/us/en/job/723072/SDE-2-RF)  
-
-  Senior Software Engineer_Design Verification----Android OR windows related experiences [Job link](https://careers.microsoft.com/us/en/job/712684/Senior-Software-Engineer-Design-Verification)  
-
-  MTE-Senior software development engineer---- Android system experiences.[Job Link](https://careers.microsoft.com/us/en/job/759956/MTE-Senior-Software-Development-Engineer-China)
-
 ## Taipei
-1. Hardware engineer
+1. Support engineer  
   
-  Firmware development Engineer-Cloud Sever Infrastructure-----BIOS/UEFI develop experiences [Job link](https://careers.microsoft.com/us/en/job/714891/Firmware-Development-Engineer-Growth-Mindset-CSI-Azure-Cloud-Server-Infrastructure-AzureHardware)   
+    Support Engineer_Security--Infra-Security [Job Link](https://careers.microsoft.com/us/en/job/787570/Support-Engineer-Security)  
+
+    Support Engineer-Azure related[Job Link](https://careers.microsoft.com/us/en/job/808501/Support-Escalation-Engineer-Azure-Rapid-Response)  
+
+    Support Engineer-Azure AD [Job Link](https://careers.microsoft.com/us/en/job/787571/Support-Engineer-Azure-AD)  
+
+    Support Escalation Engineer_High Performance Computing [Job Link](https://careers.microsoft.com/us/en/job/791592/Support-Escalation-Engineer-High-Performance-Computing)
+
+
   
-  Firmware Development Engineer(BMC)----/BMC(Baseboard management Controller) develop experiences [Job Link](https://careers.microsoft.com/us/en/job/742181/Firmware-Development-Engineer-BMC)
   
-  Senior Hardware Engineer-Cloud Server----Server or Sever rack related debug or trouble shooting experiences.[Job link](https://careers.microsoft.com/us/en/job/742553/Senior-Hardware-Engineer-Cloud-Server)  
-
-  Manufacturing Engineer- NPI Engineering----server related experiences[Job Link](https://careers.microsoft.com/us/en/job/736809/Manufacturing-Engineer-NPI-Engineering)  
-  
-  Senior Mechanical Engineer----Product:Surface Hub.[Job Link](https://careers.microsoft.com/us/en/job/748792/Senior-Mechanical-Engineer)
-
-  Senior Q&R Engineer----Server or Server rack related.[Job Link](https://careers.microsoft.com/us/en/job/752253/Senior-Q-R-Engineer)  
-
-  Senior SW Program manager(clould server). [Job Link](https://careers.microsoft.com/us/en/job/759038/Senior-SW-Program-manager-clould-server)  
-
-  Sr. Hardware System Engineer(Cloud Server). [Job link](https://careers.microsoft.com/us/en/job/759035/Sr-Hardware-System-Engineer-Cloud-Server)   
-
-2. Software engineer   
-
-  System engineer-Full stack [Job Link](https://careers.microsoft.com/us/en/job/731505/System-Software-Engineer)  
-
-  Windows Hello Engineer-C++ [Job Link](https://careers.microsoft.com/us/en/job/738111/Software-Engineer-Windows-Hello)  
-
-  Full stack engineer [Job Linke](https://careers.microsoft.com/us/en/job/749900/Full-Stack-Software-Engineer)
-
-
-
-
-## Shanghai  
-
-
-
-## Beijing  
-  
-1. Software engineer  
-  
-  software engineer----Microsoft AI framework team, develop deep learning runtime system, C/C++ [Job Link](https://careers.microsoft.com/us/en/job/728512/Senior-Software-Engineer)
   
 
-## Shenzhen
-1. Hardware engineer  
 
-  Sr. Technical Operation Engineer-Factory Management----PSU or cable connector related [Job Link](https://careers.microsoft.com/us/en/job/700303/Sr-Technical-Operation-Engineer-Factory-Management)
+
+
+
+
+  
+
+
 `;
